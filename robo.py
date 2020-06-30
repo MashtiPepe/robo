759c9d6f59e6cb6ba74f4fcd5f42953118af073f
MashtiPepe <amir@tisfoon.com>
2020-06-30 13:51:22 -0400

    update

--- robo.py
+++ robo.py
@@ -104,7 +104,7 @@ radian_to_degrees = 180 / math.pi
 #counts_180 = this is 843   R * CPR / 2 / rw
 #counts_180 = 815
 #counts_180 = 825
-counts_180 = 817
+counts_180 = 791
 
 counts_limit = world_size * 10 * CPR_div_2_pi_rw
 
