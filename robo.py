0a3f6260c14c54a9418f5d970ceb2b087f8d10d7
MashtiPepe <amir@tisfoon.com>
2020-07-01 17:12:51 -0400

    update

--- robo.py
+++ robo.py
@@ -138,7 +138,7 @@ def polar_theta(PLeft, PRight, last_PLeft, last_PRight):
     if (l_travel < 4 and r_travel > 4) or (r_travel < 4 and l_travel > 4):
       robo_theta *= 2
       print('double angle', r_travel, l_travel)
-    elif (l_travel > 0) or (r_travel > 0)
+    elif (l_travel > 0) or (r_travel > 0):
       print(r_travel, l_travel)
   
   
